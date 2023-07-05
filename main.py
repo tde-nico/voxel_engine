235f6f159c60844ae3e4dabf3c58a1c5e4e8484d
tde-nico <101901057+tde-nico@users.noreply.github.com>
2023-07-05 12:51:59 +0200

    first_chunk

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from shader_program import ShaderProgram
 from scene import Scene
 from player import Player
+from textures import Textures
 
 
 class VoxelEngine:
@@ -33,6 +34,7 @@ class VoxelEngine:
 		self.on_init()
 
 	def on_init(self):
+		self.textures = Textures(self)
 		self.player = Player(self)
 		self.shader_program = ShaderProgram(self)
 		self.scene = Scene(self)
